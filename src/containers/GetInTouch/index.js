579a0ce4cd5d7637e09fe9ecbdaf75adb2895412
Yura <yura.dev00@gmail.com>
2018-03-01 00:57:09 +0800

    Update get in touch screen content

--- src/containers/GetInTouch/index.js
+++ src/containers/GetInTouch/index.js
@@ -1,15 +1,27 @@
 import React, { Component } from 'react'
 import { Content } from '../../components/General'
+import { Title, H1, H2, Span } from '../../components'
+import styled from 'styled-components'
 
 class GetInTouch extends Component {
   render () {
     return (
       <Content>
-        Email: nomismafoundation@gmail.com
-Slack link - put as hypertext: https://join.slack.com/t/nomismaworkspace/shared_invite/enQ tMzIxNjk5ODExNDc1LWUwOGI2ODk4NzJiMjUzYWRlYj c1MTgxMGE4MmY3NWM2NTJkNDEwNjU4ODU4ZjI4N2 YwOTVhNDg4Y2NjOTE0NTI
+        <Title> Get In Touch </Title>
+          <H1>Email: nomismafoundation@gmail.com</H1>
+          <Link href='https://join.slack.com/t/nomismaworkspace/shared_invite/enQtMzIxNjk5ODExNDc1LWUwOGI2ODk4NzJiMjUzYWRlYjc1MTgxMGE4MmY3NWM2NTJkNDEwNjU4ODU4ZjI4N2YwOTVhNDg4Y2NjOTE0NTI' target="_blank">Slack link </Link>
       </Content>
     )
   }
 }
+
+const Link = styled.a`
+  margin: 0;
+  font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
+  font-weight: normal;
+  font-size: 1.8em;
+  letter-spacing: .025em;
+  padding: 10px;
+`
 
 export default GetInTouch
