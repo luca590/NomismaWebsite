b0084ebdcca6f8e4dbbc8ec5211b205c8924cd94
Yura <yura.dev00@gmail.com>
2018-03-01 00:35:36 +0800

    Update who we are screen content

--- src/components/ProfileCard/index.js
+++ src/components/ProfileCard/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import styled from 'styled-components'
 
 export default class ProfileCard extends React.Component {
 
@@ -9,6 +10,8 @@ export default class ProfileCard extends React.Component {
       expanded: false,
     };
   }
+
+ 
 
   handleExpandChange = (expanded) => {
     this.setState({expanded: expanded});
@@ -28,19 +31,56 @@ export default class ProfileCard extends React.Component {
 
   render() {
     return (
-      <Card expanded={this.state.expanded} onExpandChange={this.handleExpandChange}>
+      <Card
+        expanded={this.state.expanded}
+        onExpandChange={this.handleExpandChange}
+        style={styles.card}
+      >
         <CardHeader
           title={this.props.name}
           subtitle={this.props.subtitle}
-          avatar={this.props.picURL}
+          avatar={<img src={this.props.picURL} style={styles.avatar} />}
           actAsExpander={true}
           showExpandableButton={true}
+          titleStyle={styles.title}
+          subtitleStyle={styles.subtitle}
         />
         <CardTitle title="" subtitle={this.props.subtitle} expandable={true} />
         <CardText expandable={true}>
-          {this.props.children}
+          <Overview>
+            {this.props.children}
+          </Overview>
         </CardText>
       </Card>
     );
+  }
+}
+
+const Overview = styled.h1`
+  margin: 0;
+  font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
+  font-weight: normal;
+  font-size: 1.2em;
+  letter-spacing: .025em;
+  padding: 10px;
+`
+
+const styles = {
+  card: {
+    margin: 10
+  },
+  avatar: {
+    width: 100,
+    height: 100,
+    resizeMode: 'contain',
+    borderRadius: 50,
+    backgroundSize: 'cover'
+  },
+  title: {
+    fontSize: 25,
+    padding: 20
+  },
+  subtitle: {
+    fontSize: 20
   }
 }
