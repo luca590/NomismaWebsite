d521706b9d30ce0586b26bf88950f71c4b7784a3
Yura <yura.dev00@gmail.com>
2018-03-01 00:41:24 +0800

    Update what we do screen content

--- src/containers/WhatWeDo/index.js
+++ src/containers/WhatWeDo/index.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react'
 import { Content } from '../../components/General'
+import { Title, H1, H2, Span } from '../../components'
 
 class WhatWeDo extends Component {
   render () {
     return (
       <Content>
-        <h1>• Risk sharing and diversification are core concepts in finance. A fundamental application of finance theory is the creation of tools to customize desired cash flow/investment profiles in line with the risk tolerances of investors.</h1>
-        <h1>• The Nomisma Foundation is a new platform designed to structure and price contingent claims that span the cryptocurrency state space and thus generate fixed income and option claims.</h1>
-        <h1>• These claims allow users to match their desired cash flow / risk return profile commensurate to the risk they are willing to undertake.</h1>
+        <Title> What we do </Title>
+          <H1>• Risk sharing and diversification are core concepts in finance. A fundamental application of finance theory is the creation of tools to customize desired cash flow/investment profiles in line with the risk tolerances of investors.</H1>
+          <H1>• The Nomisma Foundation is a new platform designed to structure and price contingent claims that span the cryptocurrency state space and thus generate fixed income and option claims.</H1>
+          <H1>• These claims allow users to match their desired cash flow / risk return profile commensurate to the risk they are willing to undertake.</H1>
       </Content>
     )
   }
