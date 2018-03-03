893b9c99770b0fc762fe4c6fdc10cb37ba14727d
Yura <yura.dev00@gmail.com>
2018-03-03 09:21:15 +0800

    What sets us apart screen content update

--- src/containers/WhatWeDo/index.js
+++ src/containers/WhatWeDo/index.js
@@ -1,17 +1,42 @@
 import React, { Component } from 'react'
 import { Content } from '../../components/General'
-import { PageTitle, H1 } from '../../components'
+import { TGTitle, TGDescription, TGDetail } from '../../components'
 
 class WhatWeDo extends Component {
   render () {
     return (
       <Content>
-        <PageTitle> What we do </PageTitle>
-        <H1> Risk sharing and diversification are core concepts in finance. A fundamental application of finance theory is the creation of tools to customize desired cash flow/investment profiles in line with the risk tolerances of investors.</H1>
-        <H1> The Nomisma Foundation is a new platform designed to structure and price contingent claims that span the cryptocurrency state space and thus generate fixed income and option claims.</H1>
-        <H1> These claims allow users to match their desired cash flow / risk return profile commensurate to the risk they are willing to undertake.</H1>
+        <TGTitle> Stability <span style={styles.italic}> where it is least expected </span> </TGTitle>
+        <div style={styles.content}>
+          <div style={styles.description}>
+            <TGDescription>
+            Risk sharing and diversi cation are core concepts in  nance. A fundamental application of  nance theory is the creation of tools to customize desired cash  ow/investment pro les in line with the risk tolerances of investors.
+            </TGDescription>
+          </div>
+          <div style={styles.detail}>
+            <TGDetail>
+              The Nomisma Foundation is a new platform designed to structure and price contingent claims that span the cryptocurrency state space and thus generate fixed income and option claims. These claims allow users to match their desired cash flow / risk return profile commensurate to the risk they are willing to undertake.
+            </TGDetail>
+          </div>
+        </div>
       </Content>
     )
+  }
+}
+
+const styles = {
+  content: {
+    flex: 1,
+    flexDirection: 'column'
+  },
+  description: {
+    flex: 1
+  },
+  detail: {
+    flex: 2
+  },
+  italic: {
+    fontStyle: 'italic'
   }
 }
 
