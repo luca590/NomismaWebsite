7f6443193f6b9ac469123fb00240ca667a87f596
Michel Stekelenburg <michelstekelen@gmail.com>
2018-03-06 21:39:26 +0100

    Build main component;

---
+++ src/containers/Main/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import Section1 from './components/Section1'
+
+class Main extends Component {
+  render () {
+    return (
+      <div>
+        <Section1 />
+      </div>
+    )
+  }
+}
+
+export default Main
