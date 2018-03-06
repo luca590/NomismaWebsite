26b22a6b6735849864a32bc96ceece10321c4b8c
Michel Stekelenburg <michelstekelen@gmail.com>
2018-03-06 22:24:39 +0100

    Clean page package file;

--- src/containers/index.js
+++ src/containers/index.js
@@ -1,9 +1,1 @@
-export { default as Home } from './Home'
-export { default as WhoWeAre } from './WhoWeAre'
-export { default as WhatWeDo } from './WhatWeDo'
-export { default as HowItWorks } from './HowItWorks'
-export { default as WhitePaper } from './WhitePaper'
-export { default as Blog } from './Blog'
-export { default as GetInTouch } from './GetInTouch'
-
 export { default as Main } from './Main'
