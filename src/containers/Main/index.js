22cc45c2ebd04cc51c7f12c2d023b20a7f290df9
Yura <yura.dev00@gmail.com>
2018-03-07 09:24:58 +0800

    Section4

--- src/containers/Main/index.js
+++ src/containers/Main/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import Section1 from './components/Section1'
 import Section2 from './components/Section2'
 import Section3 from './components/Section3'
+import Section4 from './components/Section4'
 
 class Main extends Component {
   render () {
@@ -10,6 +11,7 @@ class Main extends Component {
         <Section1 />
         <Section2 />
         <Section3 />
+        <Section4 />
       </div>
     )
   }
