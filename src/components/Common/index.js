5f9ba02401a5f053617d65a52ef041ab0651cdd9
Michel Stekelenburg <michelstekelen@gmail.com>
2018-03-07 03:05:52 +0100

    Build common components;

---
+++ src/components/Common/index.js
@@ -0,0 +1,29 @@
+import styled from 'styled-components'
+
+export const LogoImage = styled.img`
+  position: absolute;
+  top: 50px;
+  left: 50px;
+`
+
+export const LogoText = styled.img`
+  position: absolute;
+  top: 70px;
+  right: 70px;
+`
+
+export const ContactLink = styled.a`
+  position: absolute;
+  bottom: 100px;
+  left: 40px;
+  transform: rotate(-90deg);
+  color: white;
+`
+
+export const InsightsLink = styled.a`
+  position: absolute;
+  bottom: 100px;
+  right: 40px;
+  transform: rotate(90deg);
+  color: white;
+`
