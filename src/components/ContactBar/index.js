ca0507085f604542d45b3d325eadd7064dfb8b1f
Michel Stekelenburg <michelstekelen@gmail.com>
2018-03-07 04:00:09 +0100

    Build contact bar component;

---
+++ src/components/ContactBar/index.js
@@ -0,0 +1,75 @@
+import React from 'react'
+import styled from 'styled-components'
+import { ContactLink } from '..'
+
+const StyledLink = styled.a`
+  margin-left: 20px;
+  margin-right: 20px;
+  font-size: 50px;
+  color: #FFF;
+`
+
+const CloseButton = styled.a`
+  position: absolute;
+  right: 100px;
+  font-size: 50px;
+  color: #FFF;
+`
+
+class ContactBar extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      isOpen: false
+    }
+  }
+
+  render () {
+    const { isOpen } = this.state
+    return (
+      <div>
+        {
+          !isOpen &&
+          <ContactLink onClick={() => { this.setState({ isOpen: true }); }}>CONTACT</ContactLink>
+        }
+        {
+          isOpen &&
+          <div style={styles.container}>
+            <div style={styles.wrapper}>
+              <StyledLink href="#">
+                <i className="fa fa-envelope-o"></i>
+              </StyledLink>
+              <StyledLink href="#">
+                <i className="fa fa-phone"></i>
+              </StyledLink>
+              <StyledLink href="#">
+                <i className="fa fa-map-marker"></i>
+              </StyledLink>
+              <CloseButton onClick={() => { this.setState({ isOpen: false }); }}>
+                <i className="fa fa-close"></i>
+              </CloseButton>
+            </div>
+          </div>
+        }
+      </div>
+    )
+  }
+}
+
+const styles = {
+  container: {
+    position: 'absolute',
+    left: 0,
+    bottom: 50,
+    paddingTop: 100,
+    paddingBottom: 100,
+    width: 800,
+    backgroundColor: '#373536',
+    textAlign: 'center'
+  },
+  wrapper: {
+    position: 'relative'
+  }
+}
+
+export default ContactBar
