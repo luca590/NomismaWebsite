4f0ffa88bad3ebe1c67a42a98e819ad11e6e80ab
Michel Stekelenburg <michelstekelen@gmail.com>
2018-03-09 01:56:42 +0100

    Background image responsive;

--- src/components/Content/index.js
+++ src/components/Content/index.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components'
 
 export const Content = styled.div`
-  width: 100%;
   position: relative;
-  background-image: url(${props => props.url})
+  background-image: url(${props => props.url});
+  background-size: auto 100%;
 `
