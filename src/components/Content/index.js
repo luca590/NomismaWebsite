0c5a4397fc99efe8215f54fbb1b6d7a2f22e32a3
Michel Stekelenburg <michelstekelen@gmail.com>
2018-03-09 03:04:14 +0100

    Make background image responsive using javascript;

--- src/components/Content/index.js
+++ src/components/Content/index.js
@@ -1,7 +1,44 @@
-import styled from 'styled-components'
+import React, { Component } from 'react'
 
-export const Content = styled.div`
-  position: relative;
-  background-image: url(${props => props.url});
-  background-size: auto 100%;
-`
+export class Content extends Component {
+  constructor(props) {
+    super(props)
+    this.imageRate = 0.625
+  }
+
+  componentDidMount() {
+    window.addEventListener('resize', this.onChangeDimensions)
+    const self = this
+    const image = new Image()
+    image.src = this.props.url
+    image.onload = function () {
+      self.imageRate = this.height / this.width
+      self.onChangeDimensions()
+    }
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.onChangeDimensions)
+  }
+
+  onChangeDimensions = () => {
+    const element = document.getElementById(this.props.id)
+    element.style.position = 'relative'
+    element.style.backgroundImage = `url(${this.props.url})`
+    if (element.offsetHeight < element.offsetWidth * this.imageRate) {
+      element.style.backgroundSize = '100% auto'
+    } else {
+      element.style.backgroundSize = 'auto 100%'
+    }
+  }
+
+  render() {
+    return (
+      <div id={this.props.id}>
+        {this.props.children}
+      </div>
+    )
+  }
+}
+
+export default Content
