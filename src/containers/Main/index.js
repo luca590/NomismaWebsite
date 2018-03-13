6bc2013fa45f29971250a24d9a8d621ac3419557
Michel Stekelenburg <michelstekelen@gmail.com>
2018-03-13 04:28:04 +0100

    Place static table on page;

--- src/containers/Main/index.js
+++ src/containers/Main/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import StaticTable from './components/StaticTable'
 import Section1 from './components/Section1'
 import Section2 from './components/Section2'
 import Section3 from './components/Section3'
@@ -8,6 +9,7 @@ class Main extends Component {
   render () {
     return (
       <div>
+        <StaticTable />
         <Section1 />
         <Section2 />
         <Section3 />
