b653c27e53a6d8b4cf3e3e3a7321a0288a30dcdc
Yura <yura.dev00@gmail.com>
2018-03-13 10:05:54 +0800

    Add react-rt-chart

--- src/containers/Main/components/Section0/index.js
+++ src/containers/Main/components/Section0/index.js
@@ -9,6 +9,7 @@ import {
   TGMenuItem,
   TGTitle
 } from '../../../../components'
+import RTChart from 'react-rt-chart'
 
 import './style.scss'
 
@@ -21,25 +22,12 @@ class Section0 extends Component {
           <TGMenuItem> Nomisma </TGMenuItem>
         </Aside>
         <div className='content'>
-          <div className='title'>
-            <TGTitle> Risk sharing revolutionizes the cryptocurrency space </TGTitle>
-          </div>
-          <div className='body'>
-            <div className='description'>
-              <TGDescription className='subTitle'>
-                Imperfect as our financial system is, I still find myself admiring it for what it does and imagining how much more impressive it can be in the future.”
-              </TGDescription>
-              <TGNote>
-                Finance and the Good Society, Robert Shiller.
-              </TGNote>
-            </div>
-            <div className='detail'>
-              <TGDetail>
-                Risk sharing is a cornerstone of finance. Cryptocurrency adoption is hampered by extreme volatility in cryptocurrency prices.
-                We are the first to bring a risk sharing solution to the crypto universe. Our key innovation is to use seasoned finance applications and deep financial insights coupled with blockchain technology. If our approach is successful, it is revolutionary for the blockchain ecosystem. Our platform promotes financial system-wide optimal risk sharing by designing and customizing portfolios that deliver fixed income equivalent cash flow profiles with collateralized cryptocurrency underlyings. Our solution makes cryptocurrencies a viable asset class for all investors.
-              </TGDetail>
-            </div>
-          </div>
+          <RTChart
+            fields={['Car','Bus']}
+            data={data}
+            maxValues={10}
+            // flow={flow}
+          />
         </div>
       </Content>
     )
